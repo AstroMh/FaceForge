ce8eddd4df72373ed525b105a90a9580d600d61f
AstroMh <mohammad.hemmat84@yahoo.com>
2025-03-16 16:54:51 +0300

    Adding Files

--- Project/config/config.py
+++ Project/config/config.py
@@ -4,6 +4,6 @@ BUFFER_SIZE = 60000
 EPOCHS = 50
 noise_dim = 100
 num_examples_to_generate = 16
-celeba_dir = r"C:\Users\moham\Downloads\archive\img_align_celeba\img_align_celeba"
+celeba_dir = r"C:\Users\moham\PycharmProjects\FaceForge\Project\data\img_align_celeba"
 checkpoint_dir = './training_checkpoints'
 checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
